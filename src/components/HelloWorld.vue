a3d3e7b239e2a8a54ef2662d73cdb21d8591b481
quang dang <truongquangdang97@gmail.com>
2022-04-27 09:47:01 +0700

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,46 +1,127 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div>
+      <h1>Lọc theo nhân sự theo  tên</h1>
+      <input type="text" placeholder="Nhập title" v-model="searchObj.name" >
+      <button @click="searchNew()">Search</button>
+    </div>
+    <h1>Danh sách nhân sự </h1>
+    <table class="table" style="margin: auto">
+      <thead>
+        <tr>
+          <th>Id</th>
+          <th>Name</th>
+          <th>Wage</th>
+       
+          <th>Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item, index) in data" :key="index">
+          <th>{{ item.id }}</th>
+          <th>{{ item.name }}</th>
+          <th>{{ item.wage }}</th>
+          <th>
+            <button @click="deleteNew(item.id)">delete</button>
+            <button @click="editNew(item.id)">edit</button>
+          </th>
+        </tr>
+      </tbody>
+    </table>
+    <div>
+      <h1>Thêm Nhân sự </h1>
+      <button v-if="obj.id > 0" @click="updateNew(obj.id)">Update</button>
+      <button v-else  @click="addNew()">Add</button>
+      <label for="">name</label>
+      <input type="text" v-model="obj.name" />
+
+      <label for="">Wage</label>
+      <input type="number" v-model="obj.wage" />
+
+    </div>
   </div>
 </template>
-
 <script>
+import axios from "axios";
+
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+  data() {
+    return {
+      data: [],
+      obj: {
+        name: "",
+        wage: "",
+      },
+      action: false,
+      searchObj:{
+        name:'',
+      }
+    };
+  },
+  created() {
+    this.all();
+  },
+  methods: {
+    searchNew(){
+      axios
+        .get("http://localhost:8080/api/v1/employees?name="+this.searchObj.name)
+        .then((response) => (this.data = response.data));
+    },
+
+    all() {
+      axios
+        .get("http://localhost:8080/api/v1/employees")
+        .then((response) => (this.data = response.data));
+    },
+    addNew() {
+      axios
+        .post("http://localhost:8080/api/v1/employees", this.obj)
+        .then((response) => {
+          console.log(response);
+          this.all();
+          this.obj = {};
+        });
+    },
+    deleteNew(id) {
+      console.log(id);
+      axios
+        .delete("http://localhost:8080/api/v1/employees/" + id)
+        .then((response) => {
+          console.log(response);
+          this.all();
+          this.obj = {};
+        });
+    },
+    editNew(id) {
+      axios.get("http://localhost:8080/api/v1/employees/" + id).then((response) => {
+
+        this.obj = response.data;
+        console.log(this.obj);
+      });
+    },
+    updateNew(id) {
+      axios
+        .put("http://localhost:8080/api/v1/employees/" + id, this.obj)
+        .then((response) => {
+          console.log(response);
+          this.all();
+          this.obj = {};
+        });
+      this.obj = [];
+    },
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+
 <style scoped>
+.hello{
+  text-align: center;
+}
 h3 {
   margin: 40px 0 0;
 }
